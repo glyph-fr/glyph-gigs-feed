c862d2c145db8fe1c2c3aff26fc6de170e594f29
vala <gonjo@free.fr>
2013-12-30 10:57:09 +0100

    push site

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+GlyphGigsFeed::Application.config.secret_key_base = 'dd06c098ac2e6ad2a9b7387527384709474509d3b5569f7d163ce263fbb938e45fb566df612212b9411d89f0c406efdd476e98b9a33a54b80376d3c54b01e0b0'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+GlyphGigsFeed::Application.config.session_store :cookie_store, key: '_glyph-gigs-feed_session'
